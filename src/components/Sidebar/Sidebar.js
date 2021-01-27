14c42be6ba95f3f9851d3e5639638b2a4ebede40
unknown <oboreisa@gmail.com>
2021-01-27 17:20:20 +0200

    converting sidebar text-categories into react links

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,20 +1,22 @@
 import React, {useState} from 'react'
 import classes from './Sidebar.module.css'
+import {Link} from "react-router-dom";
+
 
 
 const Sidebar = () => {
-    const [modules, setModules] = useState (
+    const [categories, setCategories] = useState (
         [
-            {id: 1, cat: 'Art'},
-            {id: 2, cat: 'Citizenship'},
-            {id: 3, cat: 'Geography'},
-            {id: 4, cat: 'History'},
-            {id: 5, cat: 'Languages'},
-            {id: 6, cat: 'Literacy'},
-            {id: 7, cat: 'Music'},
-            {id: 8, cat: 'Physical Education'},
-            {id: 9, cat: 'Science'},
-            {id: 10, cat: 'Information Technology'}
+            {id: 1, path: '/art', cat: 'Art'},
+            {id: 2, path: '/citizenship', cat: 'Citizenship'},
+            {id: 3, path: '/geography', cat: 'Geography'},
+            {id: 4, path: '/history', cat: 'History'},
+            {id: 5, path: '/languages', cat: 'Languages'},
+            {id: 6, path: '/literacy', cat: 'Literacy'},
+            {id: 7, path: '/music', cat: 'Music'},
+            {id: 8, path: '/pe', cat: 'Physical Education'},
+            {id: 9, path: '/science', cat: 'Science'},
+            {id: 10, path: '/it', cat: 'Information Technology'}
         ]
     )
 
@@ -23,8 +25,9 @@ const Sidebar = () => {
             <h3 style={{textDecoration: "underline"}}>Categories</h3>
             <div className={classes.cat}>
                 {
-                    modules.map((sub) => (
-                        <p key={sub.id}>{sub.cat}</p>
+                    categories.map((cat) => (
+                        <Link to={cat.path} key={cat.id} className={classes.link}>{cat.cat}</Link>
+
                     ))
                 }
             </div>
