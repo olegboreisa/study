e3911fe1bb6a682179b5c8a01c1bd9aaa42c79ca
unknown <oboreisa@gmail.com>
2021-02-01 14:08:00 +0200

    adding security dependency and creating security folder with first filter

---
+++ backend/src/main/java/lt/boreisa/backend/security/JwtAuthenticationFilter.java
@@ -0,0 +1,10 @@
+package lt.boreisa.backend.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * [Užklausų perėmimą. Pabandymą autentifikuoti asmenį] -> JwtAuthenticationFilter
+ */
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+}
