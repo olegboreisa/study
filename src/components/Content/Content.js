5db8b83c5119b1c2617396bcccad2fbf4bf94dd5
unknown <oboreisa@gmail.com>
2021-02-04 09:39:42 +0200

    added login component and route

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -7,17 +7,19 @@ import CategoryAdd from "../Sidebar/AddCategory";
 import ArticleAdd from "./pages/Articles/Article/AddArticle";
 import SingleArtById from "./pages/Articles/Article/Article";
 import UpdateArt from "./pages/Articles/Article/UpdateArticle";
+import Login from './pages/Login/Login'
 
 
 const Content = () => (
 
         <Switch>
-            <Route path="/api/articles/update/:id" component={UpdateArt} />
             <Route path="/about" component={About} />
             <Route path="/contacts" component={Contacts} />
+            <Route path="/login" component={Login} />
             <Route path="/categories/add" component={CategoryAdd} />
             <Route path="/api/articles/:id" component={SingleArtById} />
             <Route path="/articles/add" component={ArticleAdd} />
+            <Route path="/api/articles/update/:id" component={UpdateArt} />
             <Route exact path="/" component={Categories}/>
         </Switch>
 
