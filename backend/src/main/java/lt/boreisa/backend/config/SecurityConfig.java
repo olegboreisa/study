365b91567eef3eb76cac9005ef28d81014846329
unknown <oboreisa@gmail.com>
2021-02-04 09:07:27 +0200

    finished with backend security configurations

--- backend/src/main/java/lt/boreisa/backend/config/SecurityConfig.java
+++ backend/src/main/java/lt/boreisa/backend/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -40,7 +41,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and()
                 .addFilter(new JwtAuthenticationFilter(authenticationManager(), jwtProvider))
                 .addFilter(new JwtAuthorizationFilter(authenticationManager(), jwtProvider));
-
     }
 
     @Bean
@@ -57,6 +57,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder encoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity webSecurity) {
+        webSecurity.ignoring().antMatchers("/h2/**");
     }
 
 }
