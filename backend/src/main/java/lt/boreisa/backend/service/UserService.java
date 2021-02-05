a550ce8bf59723684abc2c340a8b04b23a1ba22e
unknown <oboreisa@gmail.com>
2021-02-05 23:39:28 +0200

    adding passwordencoder service into userservice

--- backend/src/main/java/lt/boreisa/backend/service/UserService.java
+++ backend/src/main/java/lt/boreisa/backend/service/UserService.java
@@ -3,17 +3,21 @@ package lt.boreisa.backend.service;
 import lt.boreisa.backend.model.DTO.UserRegisterDTO;
 import lt.boreisa.backend.model.User;
 import lt.boreisa.backend.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService implements UserDetailsService {
 
-    private UserRepo userRepo;
+    private final UserRepo userRepo;
+    private PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, PasswordEncoder passwordEncoder) {
         this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,9 +28,10 @@ public class UserService implements UserDetailsService {
 
     public User getUserFromRegistration(UserRegisterDTO userRegisterDTO) {
         User user = new User();
+        String encodedPassword = passwordEncoder.encode(userRegisterDTO.getPassword());
         user.setUsername(userRegisterDTO.getUsername());
-        user.setPassword(userRegisterDTO.getPassword());
-        user.setMatchPassword(userRegisterDTO.getMatchPassword());
+        user.setPassword(encodedPassword);
+        user.setMatchPassword(encodedPassword);
         user.setCountry(userRegisterDTO.getCountry());
         user.setPhoneNum(userRegisterDTO.getPhoneNum());
         return userRepo.save(user);
