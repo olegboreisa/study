4175b6e8ca1a0fdca8aaa591639986ba96fa21d9
unknown <oboreisa@gmail.com>
2021-02-05 23:11:42 +0200

    adding userdto to catch data from register form

---
+++ backend/src/main/java/lt/boreisa/backend/model/DTO/UserRegisterDTO.java
@@ -0,0 +1,17 @@
+package lt.boreisa.backend.model.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties
+public class UserRegisterDTO {
+
+    private Long id;
+    private String username;
+    private String password;
+    private String matchPassword;
+    private String country;
+    private String phoneNum;
+    
+}
