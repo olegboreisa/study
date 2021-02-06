9aab9a35f5c62dbc991ea1f47f2f3d113e9398e6
unknown <oboreisa@gmail.com>
2021-02-06 20:28:05 +0200

    making sidebar international and responsive to user login

--- src/api/CategoryApi.js
+++ src/api/CategoryApi.js
@@ -1,5 +1,5 @@
 import HTTP from './Main'
 
-export const getCategories = () => HTTP.get('/categories')
-
-export const deleteCategory = (id) => HTTP.delete(`/categories/delete/${id}`)
+// export const getCategories = () => HTTP.get('/categories')
+//
+// export const deleteCategory = (id) => HTTP.delete(`/categories/delete/${id}`)
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,35 +1,46 @@
 import React, {useEffect, useState} from 'react'
 import classes from './Sidebar.module.css'
 import {Link} from "react-router-dom";
-import {faPlus, faMinus} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
 
 const Sidebar = () => {
+
+    const { t } = useTranslation("categories")
+    const user = useSelector(state => state.user.userData)
+
     const categories = [
-        {id: 1, path: '/articles/biology', name: "Biology"},
-        {id: 2, path: '/articles/chemistry', name: "Chemistry"},
-        {id: 3, path: '/articles/art', name: "Art"},
-        {id: 4, path: '/articles/music', name: "Music"},
-        {id: 5, path: '/articles/mathematics', name: "Mathematics"}
+        {id: 1, path: '/articles/biology', name: `${t('biology')}`},
+        {id: 2, path: '/articles/chemistry', name: `${t('chemistry')}`},
+        {id: 3, path: '/articles/art', name: `${t('art')}`},
+        {id: 4, path: '/articles/music', name: `${t('music')}`},
+        {id: 5, path: '/articles/mathematics', name: `${t('mathematics')}`}
     ]
 
-
     return (
-        <div className={classes.container}>
-            <h3 style={{textDecoration: "underline"}}>Categories</h3>
-            <div className={classes.cat}>
-                {
-                    categories.map(cat => (
-                        <div className={classes.wrapper} key={cat.id}>
-
-                            <Link to={cat.path} className={classes.link}>{cat.name}</Link>
-
-                        </div>
-                    ))
-                }
+        user === null ?
+            (
+            <div className={classes.container}>
+                <h5>{t('sign')}</h5>
             </div>
-        </div>
+            )
+            :
+            (
+                <div className={classes.container}>
+                    <h3 style={{textDecoration: "underline"}}>{t('category')}</h3>
+                    <div className={classes.cat}>
+                        {
+                            categories.map(cat => (
+                                <div className={classes.wrapper} key={cat.id}>
 
+                                    <Link to={cat.path} className={classes.link}>{cat.name}</Link>
+
+                                </div>
+                            ))
+                        }
+                    </div>
+                </div>
+            )
     )
 }
 
